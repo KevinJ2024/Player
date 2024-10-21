9f9642fb7a608eebc0ff15a024e1edee620925ff
SENA <SENA@ARMSCCCCTGOD177.FormacionSena.RED>
2024-10-21 15:48:27 -0500

    finished work, adding songs

---
+++ songs.js
@@ -0,0 +1,32 @@
+export const playList = [
+    {
+        title: "Lo malo de ser bueno",
+        artist: "Cuarteto de Nos",
+        img: "assets/images/img1.jfif", 
+        song: "assets/audio/Lo_malo_de_ser_bueno_[_YouConvert.net_].mp3" 
+    },
+    {
+        title: "Lamento Boliviano",
+        artist: "Enanitos verdes",
+        img: "assets/images/img2.jfif",
+        song: "assets/audio/Lamento_Boliviano_[_YouConvert.net_].mp3"
+    },
+    {
+        title: 'Song 3',
+        artist: 'Artist 3',
+        img: 'ruta/de/la/imagen3.jpg',
+        song: 'ruta/de/la/cancion3.mp3'
+    },
+    {
+        title: 'Song 4',
+        artist: 'Artist 4',
+        img: 'ruta/de/la/imagen4.jpg',
+        song: 'ruta/de/la/cancion4.mp3'
+    },
+    {
+        title: 'Song 5',
+        artist: 'Artist 5',
+        img: 'ruta/de/la/imagen5.jpg',
+        song: 'ruta/de/la/cancion5.mp3'
+    },
+];
